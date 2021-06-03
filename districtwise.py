fd52c6f2f220ca4a29629e15f75376eb4ce0301c
Pushpak <50488701+pushpak25w@users.noreply.github.com>
2021-06-03 17:54:15 +0530

    Delete districtwise.py

--- districtwise.py
+++
@@ -1,8 +0,0 @@
-def districtwise(crime,district):
-	fp = "gadm36_IND_shp/gadm36_IND_2.shp"
-	map_df = gpd.read_file(fp)
-	district_wise = data01to14[['STATE/UT', 'DISTRICT', crime]]
-	district_wise = district_wise[district_wise['STATE/UT']==district]
-	merged = map_df.set_index('NAME_2').join(district_wise.set_index('DISTRICT'))
-	fig, ax = plt.subplots(1, figsize=(10, 6))
-	fig.savefig("District_wise.png", dpi=100)
